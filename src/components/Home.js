73a2a18cf5d0e611ebbacf758160dcb510ccd917
poojaghodmode <poojaghodmode@gmail.com>
2021-07-12 17:37:05 +0530

    Add grid

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ import API from "../API";
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 //components
 import HeroImage from "./HeroImage";
+import Grid from "./Grid";
 //Hook
 import { useHomeFetch } from "../hooks/useHomeFetch";
 //Image
@@ -16,7 +17,6 @@ const Home = () => {
   console.log(state);
   return (
     <>
-
       {state.results[0] && (
         <HeroImage
           image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
@@ -24,6 +24,13 @@ const Home = () => {
           text={state.results[0].overview}
         />
       )}
+      <Grid header="Popular Movies">
+        {state.results.map(movie => (
+          <div key={movie.id}>{movie.title}</div>
+        ))}
+
+
+      </Grid>
     </>
   );
 };
