c4ae3230533f32813def7ec252e254d19f422aab
poojaghodmode <poojaghodmode@gmail.com>
2021-07-12 13:39:45 +0530

    fetch data from api

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,7 @@
+import { useState, useEffect } from "react";
+//API
+import API from "../API";
 //config
-import { useState } from "react";
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 //components
 
@@ -13,6 +15,29 @@ const Home = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
+  const fetehMovies = async (page, searchTerm = "") => {
+    try {
+      setError(false);
+      setLoading(true);
+      const movies = await API.fetchMovies(searchTerm, page);
+
+      setState((prev) => ({
+        ...movies,
+        results:
+          page > 1 ? [...prev.results, ...movies.results] : [...movies.results],
+      }));
+    } catch (error) {
+      setError(true);
+    }
+    setLoading(false);
+  };
+
+  // initial render
+  useEffect(() => {
+    fetehMovies(1);
+  }, []);
+ 
+console.log(state);
   return <div className="home"></div>;
 };
 
