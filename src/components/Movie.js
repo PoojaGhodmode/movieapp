4e320f3f24b20708d0019a14986b01e5abb16411
poojaghodmode <poojaghodmode@gmail.com>
2021-07-13 19:54:17 +0530

    Add movie scaffold

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,17 @@
+//config
+import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
+//Components
+import Grid from "./Grid";
+import Spinner from "./Spinner";
+//hook
+
+//Image
+import NoImage from "../images/no_image.jpg";
+
 const Movie = () => {
-  return <div>movie</div>;
+  return <>
+  
+  </>;
 };
 
 export default Movie;
